2a02e7da7ed3de7343b5a46eb48afed4da4c15e1
Ernesto Camacho <ernesto_camacho@ymail.com>
2017-03-25 02:01:07 -0600

    added notifications to admin sidebar

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,10 +3,18 @@ class Admin::BaseController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_only
   before_action :btc_price_set?
+  before_action :set_notifications, except: [:create, :update, :destroy]
 
   layout 'admin'
 
   private
+
+    def set_notifications
+      pending_orders = Order.where(submitted: true, completed: false).count
+      new_orders = Order.where(submitted: false, completed: false).count
+      messages = Message.where(admin_read: false).count
+      @notifications = pending_orders + new_orders + messages
+    end
 
     def btc_price_set?
       if !Setting.btc_price
