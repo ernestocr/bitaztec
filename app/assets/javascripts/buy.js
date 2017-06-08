e54fc52fa1a6b1f75f5abf9a4c29126cdb5a624d
Ernesto Camacho <ernesto_camacho@ymail.com>
2017-06-08 14:56:33 -0500

    added fourth buying option

--- app/assets/javascripts/buy.js
+++ app/assets/javascripts/buy.js
@@ -5,12 +5,12 @@
 
 /* Currency format */
 Number.prototype.formatMoney = function(c, d, t){
-  var n = this, 
-    c = isNaN(c = Math.abs(c)) ? 2 : c, 
-    d = d == undefined ? "." : d, 
-    t = t == undefined ? "," : t, 
-    s = n < 0 ? "-" : "", 
-    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+  var n = this,
+    c = isNaN(c = Math.abs(c)) ? 2 : c,
+    d = d == undefined ? "." : d,
+    t = t == undefined ? "," : t,
+    s = n < 0 ? "-" : "",
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
     j = (j = i.length) > 3 ? j % 3 : 0;
   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
@@ -21,10 +21,14 @@ function confirmBuy() {
   var price = $('#order_price').data('value');
   var amount = $('#order_amount').val();
   var total = price * amount;
+  var method = $('.options div[data-value="' +
+                $('#order_payment_method').val() +
+                 '"] span').text();
 
   $('.verify-price').text(price.formatMoney(0));
   $('.verify-amount').text(amount);
   $('.verify-total').text(total.formatMoney(0));
+  $('.payment-method-chosen').text(method)
 }
 
 
@@ -39,7 +43,7 @@ function submitForm() {
   // update hidden form
   var amount = $('#order_amount').val();
   $('.buy form input[name="order[amount]"]').val(amount);
-  
+
   // must be changed
   var method = $('#order_payment_method').val();
   $('.buy form select').val(method);
@@ -50,20 +54,20 @@ function submitForm() {
 
 /* Initiate flow */
 $(document).ready(function() {
-  
+
   // Only run on the buy page
   if ( !$('#new-order').length ) { return false; }
 
   var CURRENT_STEP         = 1;
   var SELECTED_METHOD      = '';
   var SELECTED_METHOD_TYPE = '';
-  
+
   // NEXT STEP
   $('.next').on('click', function() {
     if ( $('#order_amount').val() == '' ) { return false; }
-    
+
     $('.make-order').hide();
-    
+
     var amount = parseFloat( $('#order_amount_m').val().replace(',', '') );
     if ( amount < MIN ) {
       alert('La cantidad minima es $' + MIN + ' pesos.');
@@ -72,14 +76,14 @@ $(document).ready(function() {
     }
 
     if ( CURRENT_STEP == 1 ) {
-      
+
       $('.step').hide();
       $('.step-2').show();
       $('.prev').css('display', 'inline-block');
       CURRENT_STEP = 2;
-    
+
     } else if ( CURRENT_STEP == 2) {
-      
+
       if ( SELECTED_METHOD != '' ) {
         $('.step').hide();
         $('.step-3').show();
@@ -87,26 +91,26 @@ $(document).ready(function() {
         $('.make-order').css('display', 'inline-block');
         confirmBuy();
         CURRENT_STEP = 3;
-      
+
       }
     }
-    changeTab(CURRENT_STEP); 
+    changeTab(CURRENT_STEP);
   }); // END NEXT STEP
-  
+
   // GO BACK
   $('.prev').on('click', function() {
     $('.step').hide();
     $('.make-order').hide();
 
     if ( CURRENT_STEP == 2 ) {
-      
+
       $('.step-1').show();
       $('.prev').hide();
 
       CURRENT_STEP = 1;
 
     } else if ( CURRENT_STEP == 3 ) {
-      
+
       $('.step-2').show();
       $('.prev').show();
       $('.next').show();
@@ -114,7 +118,7 @@ $(document).ready(function() {
       CURRENT_STEP = 2;
     }
 
-    changeTab(CURRENT_STEP); 
+    changeTab(CURRENT_STEP);
   });
 
   // SHOW PAYMENT METHOD SUB-OPTIONS
@@ -143,4 +147,3 @@ $(document).ready(function() {
   });
 
 });
-
